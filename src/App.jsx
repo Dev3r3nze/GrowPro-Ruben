65edf4def675b68fb49712c0d98c518f7ccdb88d
dev3r3nze <dev3r3nze@gmail.com>
2023-03-27 11:42:19 +0200

    Logo visible

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <div className='header'> 
+          <img src={logo} className='logo' alt='logo growpro'></img> 
+        </div>
+        <div>
+          <img src='Resources\header.png' alt='header background'></img>
+        </div>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
